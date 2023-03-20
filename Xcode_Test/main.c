2f44ff241f59f9a3575c32daf9f96bc0de5a3006
HANSEOL <nothinonthere@icoud.com>
2023-03-20 21:35:21 +0900

    Initial Commit

---
+++ Xcode_Test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Xcode_Test
+//
+//  Created by 한설 on 2023/03/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
